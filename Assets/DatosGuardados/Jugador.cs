59e1dab821627026e0c8ee5a80448d20174af278
Byron <graninty@gmail.com>
2021-08-04 17:20:39 -0400

    aplicar bolt y 1ra creaccion de clase jugador

---
+++ Assets/DatosGuardados/Jugador.cs
@@ -0,0 +1,328 @@
+
+[System.Serializable]
+public class Jugador
+{
+    //personajes nombre:
+
+    string pj0;
+    string pj1;
+    string pj2;
+    string pj3;
+    string pj4;
+    string pj5;
+
+    //personaje class:
+    //00 : class
+
+    string pj0_c1;
+    string pj0_c2;
+
+    string pj1_c1;
+    string pj1_c2;
+
+    string pj2_c1;
+    string pj2_c2;
+
+    string pj3_c1;
+    string pj3_c2;
+
+    string pj4_c1;
+    string pj4_c2;
+
+    string pj5_c1;
+    string pj5_c2;
+
+    //personajes Talents:
+    //0xxxxx... : talento 1
+    //x0xxxx... : +stats
+    //xx0xxx... : talento 2
+    //xxxx0x... : +stats
+    //xxxxx0... : etc
+
+
+    string pj0_t1;
+    string pj0_t2;
+    string pj0_t3;
+
+    string pj1_t1;
+    string pj1_t2;
+    string pj1_t3;
+
+    string pj2_t1;
+    string pj2_t2;
+    string pj2_t3;
+
+    string pj3_t1;
+    string pj3_t2;
+    string pj3_t3;
+
+    string pj4_t1;
+    string pj4_t2;
+    string pj4_t3;
+
+    string pj5_t1;
+    string pj5_t2;
+    string pj5_t3;
+
+
+    //personajes skillDeck:
+    //00xxxx : clase
+    //xx00xx : skill
+    //xxxx00 : 
+
+    //pj0:
+    string pj0_s0;
+    string pj0_s1;
+    string pj0_s2;
+    string pj0_s3;
+    string pj0_s4;
+    string pj0_s5;
+    string pj0_s6;
+    string pj0_s7;
+    string pj0_s8;
+    string pj0_s9;
+    string pj0_s10;
+    string pj0_s11;
+    string pj0_s12;
+    string pj0_s13;
+    string pj0_s14;
+    string pj0_s15;
+    string pj0_s16;
+    string pj0_s17;
+    string pj0_s18;
+    string pj0_s19;
+
+    //pj1:
+    string pj1_s0;
+    string pj1_s1;
+    string pj1_s2;
+    string pj1_s3;
+    string pj1_s4;
+    string pj1_s5;
+    string pj1_s6;
+    string pj1_s7;
+    string pj1_s8;
+    string pj1_s9;
+    string pj1_s10;
+    string pj1_s11;
+    string pj1_s12;
+    string pj1_s13;
+    string pj1_s14;
+    string pj1_s15;
+    string pj1_s16;
+    string pj1_s17;
+    string pj1_s18;
+    string pj1_s19;
+
+    //pj2:
+    string pj2_s0;
+    string pj2_s1;
+    string pj2_s2;
+    string pj2_s3;
+    string pj2_s4;
+    string pj2_s5;
+    string pj2_s6;
+    string pj2_s7;
+    string pj2_s8;
+    string pj2_s9;
+    string pj2_s10;
+    string pj2_s11;
+    string pj2_s12;
+    string pj2_s13;
+    string pj2_s14;
+    string pj2_s15;
+    string pj2_s16;
+    string pj2_s17;
+    string pj2_s18;
+    string pj2_s19;
+
+    //pj3:
+    string pj3_s0;
+    string pj3_s1;
+    string pj3_s2;
+    string pj3_s3;
+    string pj3_s4;
+    string pj3_s5;
+    string pj3_s6;
+    string pj3_s7;
+    string pj3_s8;
+    string pj3_s9;
+    string pj3_s10;
+    string pj3_s11;
+    string pj3_s12;
+    string pj3_s13;
+    string pj3_s14;
+    string pj3_s15;
+    string pj3_s16;
+    string pj3_s17;
+    string pj3_s18;
+    string pj3_s19;
+
+    //pj4:
+    string pj4_s0;
+    string pj4_s1;
+    string pj4_s2;
+    string pj4_s3;
+    string pj4_s4;
+    string pj4_s5;
+    string pj4_s6;
+    string pj4_s7;
+    string pj4_s8;
+    string pj4_s9;
+    string pj4_s10;
+    string pj4_s11;
+    string pj4_s12;
+    string pj4_s13;
+    string pj4_s14;
+    string pj4_s15;
+    string pj4_s16;
+    string pj4_s17;
+    string pj4_s18;
+    string pj4_s19;
+
+    //pj5:
+    string pj5_s0;
+    string pj5_s1;
+    string pj5_s2;
+    string pj5_s3;
+    string pj5_s4;
+    string pj5_s5;
+    string pj5_s6;
+    string pj5_s7;
+    string pj5_s8;
+    string pj5_s9;
+    string pj5_s10;
+    string pj5_s11;
+    string pj5_s12;
+    string pj5_s13;
+    string pj5_s14;
+    string pj5_s15;
+    string pj5_s16;
+    string pj5_s17;
+    string pj5_s18;
+    string pj5_s19;
+
+    //personaje talents progress:
+    //00 : puntos de talentos
+
+    string pj0_tp;
+    string pj1_tp;
+    string pj2_tp;
+    string pj3_tp;
+    string pj4_tp;
+    string pj5_tp;
+
+    //mochila
+    //00xxxx : tipo
+    //xx00xx :
+
+    string m0;
+    string m1;
+    string m2;
+    string m3;
+    string m4;
+    string m5;
+    string m6;
+    string m7;
+    string m8;
+    string m9;
+    string m10;
+    string m11;
+    string m12;
+    string m13;
+    string m14;
+    string m15;
+    string m16;
+    string m17;
+    string m18;
+    string m19;
+
+    //materiales
+    //000 : cantidad
+
+    string ma0;
+    string ma1;
+    string ma2;
+    string ma3;
+    string ma4;
+    string ma5;
+    string ma6;
+    string ma7;
+    string ma8;
+    string ma9;
+    string ma10;
+    string ma11;
+    string ma12;
+    string ma13;
+    string ma14;
+    string ma15;
+    string ma16;
+    string ma17;
+    string ma18;
+    string ma19;
+
+    //personaje equipamiento:
+    // xxxxxxx : item
+
+    string pj0_e1;
+    string pj0_e2;
+    string pj0_e3;
+    string pj0_e4;
+    string pj0_e5;
+    string pj0_e6;
+    string pj0_e7;
+
+    string pj1_e1;
+    string pj1_e2;
+    string pj1_e3;
+    string pj1_e4;
+    string pj1_e5;
+    string pj1_e6;
+    string pj1_e7;
+
+    string pj2_e1;
+    string pj2_e2;
+    string pj2_e3;
+    string pj2_e4;
+    string pj2_e5;
+    string pj2_e6;
+    string pj2_e7;
+
+    string pj3_e1;
+    string pj3_e2;
+    string pj3_e3;
+    string pj3_e4;
+    string pj3_e5;
+    string pj3_e6;
+    string pj3_e7;
+
+    string pj4_e1;
+    string pj4_e2;
+    string pj4_e3;
+    string pj4_e4;
+    string pj4_e5;
+    string pj4_e6;
+    string pj4_e7;
+
+    string pj5_e1;
+    string pj5_e2;
+    string pj5_e3;
+    string pj5_e4;
+    string pj5_e5;
+    string pj5_e6;
+    string pj5_e7;
+
+
+    //historyQuestProgress
+    //00xxxxxx : campania
+    //xx00xxxx : quest
+    //xxxx00xx : progreso 1
+    //xxxxxx00 : progreso 2
+
+    string hqp;
+
+    //
+ 
+
+}
+
